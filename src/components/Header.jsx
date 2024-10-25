22e80ce00f5b44054c7ac3d4181bf2cc0b5bc73d
prithvijedipatil <prithvipatil09@gmail.com>
2024-10-25 16:04:12 +0530

    email

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -152,7 +152,7 @@ const Header = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
               >
-                {user && user.email === "writetoprithvipatil@gmail.com" && (
+                {user && user.email === "thebluesheeptirthan@gmail.com" && (
                   <Link to={"/createItem"}>
                     <p
                       className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
@@ -248,7 +248,7 @@ const Header = () => {
             <>
               {
                 <motion.div className=" p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base">
-                  {user && user.email === "writetoprithvipatil@gmail.com" && (
+                  {user && user.email === "thebluesheeptirthan@gmail.com" && (
                     <Link to={"/createItem"}>
                       <p className="  flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
                         New Item <MdAdd />
